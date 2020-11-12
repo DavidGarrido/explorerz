8b383bacf86bedfe04d98f44ad5a03f532acb8cd
Davidagh <www.davidalexander@gmail.com>
2020-11-12 13:57:11 -0500

    traduction y roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,11 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function roles () {
+        return $this->belongsToMany('App\Models\Role')->withTimesTamps();
+    }
+    // public function havePermission($permission){
+        
+    // }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$Lp4.3QI35z8neRB2ZhHDuepAsSgsfSm6a.lS1/ZU2FrdpbiPu/Sfi', // password
             'remember_token' => Str::random(10),
         ];
     }
